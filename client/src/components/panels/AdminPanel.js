2c98914e7c4ebf96a1acd79e774dfae6eb365582
vityokklymets <vityokklymets@gmail.com>
2018-11-13 21:18:36 +0200

    AdminPanel menu

--- client/src/components/panels/AdminPanel.js
+++ client/src/components/panels/AdminPanel.js
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Menu, Grid } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 export default () => {
+  const [activeItem, setActiveItem] = useState('add')
+  const setMenuItem = name => {
+    return {
+      name,
+      active: activeItem === name,
+      onClick: () => setActiveItem(name)
+    }
+  }
   return (
-    <div>
-      <h3>Admin panel</h3>
-    </div>
+    <Grid>
+      <Grid.Row>
+        <Grid.Column computer={4}>
+          <Menu fixed='top' vertical>
+            <Link to='/'>
+              <Menu.Item icon='home' {...setMenuItem('home')} />
+            </Link>
+            <Menu.Item>
+              Games
+              <Menu.Menu>
+                <Menu.Item icon='edit' {...setMenuItem('add')} />
+                <Menu.Item icon='search' {...setMenuItem('find')} />
+              </Menu.Menu>
+            </Menu.Item>
+          </Menu>
+        </Grid.Column>
+        <Grid.Column />
+      </Grid.Row>
+    </Grid>
   )
 }

a9d25aa3c8fb4cf860328c87ab07e8541fe17905
vityokklymets <vityokklymets@gmail.com>
2018-11-14 00:49:40 +0200

    AddGameForm

--- client/src/components/forms/AddGameForm.js
+++ client/src/components/forms/AddGameForm.js
@@ -1,44 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useInput, useSelect } from '../hooks'
-import { Form, Button, Loader } from 'semantic-ui-react'
-import { Query } from 'react-apollo'
-import { GET_CATEGORIES } from '../../queries'
-export default () => {
+import { Form, Button } from 'semantic-ui-react'
+import { Mutation } from 'react-apollo'
+import Files from 'react-files'
+import { ADD_GAME } from '../../queries'
+// TODO validation, error messages
+export default ({ categories }) => {
   const category = useSelect('')
   const name = useInput('')
   const description = useInput('')
-  const width = 8
+  const [file, setFile] = useState(null)
+  const fileHandler = files => {
+    const reader = new window.FileReader()
+    const file = files[0]
+    reader.onload = e => {
+      setFile({
+        ...file,
+        result: reader.result
+      })
+    }
+    reader.readAsText(file)
+  }
+  const submitHandle = (e, submit) => {
+    e.preventDefault()
+    submit()
+  }
   return (
-    <Query query={GET_CATEGORIES}>
-      {({ loading, data: { categories } }) => {
-        if (loading) return <Loader active inline />
+    <Mutation
+      variables={{
+        file,
+        category: category.value,
+        name: name.value,
+        description: description.value
+      }}
+      mutation={ADD_GAME}
+    >
+      {(addGame, { loading }) => {
         return (
-          <Form>
+          <Form
+            onSubmit={e => submitHandle(e, addGame)}
+            loading={loading}
+            className='GameForm'
+          >
             <Form.Input
-              width={width}
               icon='game'
               iconPosition='left'
               placeholder='Name'
               {...name}
             />
             <Form.TextArea
-              width={width}
               placeholder='enter description ....'
               {...description}
             />
             <Form.Select
               {...category}
-              width={width}
               placeholder='Category'
               options={categories}
             />
-
+            <div>
+              <Files
+                className='files-dropzone'
+                onChange={fileHandler}
+                accepts={['application/x-shockwave-flash']}
+                maxFiles={3}
+                maxFileSize={10000000}
+                minFileSize={0}
+                clickable
+              >
+                Drop files here or click to upload
+              </Files>
+            </div>
             <Button floated='left' color='green' size='medium'>
               Add
             </Button>
           </Form>
         )
       }}
-    </Query>
+
+    </Mutation>
   )
 }

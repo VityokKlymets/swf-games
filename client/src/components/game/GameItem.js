01c45e2f758881fae242952e9e4f80ccbddc44d0
vityokklymets <vityokklymets@gmail.com>
2018-11-14 13:27:18 +0200

    GameItem

---
+++ client/src/components/game/GameItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Card, Image, Label } from 'semantic-ui-react'
+export default ({ name, description, screenshot, category, createdAt }) => {
+  const date = new Date(parseInt(createdAt))
+  return (
+    <Card>
+      <Image src={screenshot} />
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>
+          <span className='date'>{date.toLocaleDateString()}</span>
+        </Card.Meta>
+        <Card.Description>{description}</Card.Description>
+        <Card.Meta textAlign='right'>
+          <Label color='green'>{category}</Label>
+        </Card.Meta>
+      </Card.Content>
+    </Card>
+  )
+}

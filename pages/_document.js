f940ec17a2fd9c506daf7f528881a87daa5a1d08
vityokklymets <vityokklymets@gmail.com>
2018-12-08 21:28:39 +0200

    add favicon , assets

--- pages/_document.js
+++ pages/_document.js
@@ -14,11 +14,21 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <title>swf-games</title>
+          <meta
+            name='google-site-verification'
+            content='pxOqYizqs4Mwj4MYqQkVZJ_SZADolF1JCsBsjPYqEd4'
+          />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1, shrink-to-fit=no'
+          />
           <link
             rel='stylesheet'
             href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css'
           />
           <link rel='stylesheet' href='/static/css/index.css' />
+          <link rel='shortcut icon' href='/static/favicon.png' />
         </Head>
         <body>
           <Main />
